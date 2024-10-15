9fa5e4387436acab25406a5e4e505b70347d74d3
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-10-15 17:28:09 -0500

    Deleted some logs in the browser

--- src/hooks/useGetWeather.js
+++ src/hooks/useGetWeather.js
@@ -26,7 +26,6 @@ export default function useGetWeather() {
     try {
       const response = await fetch(baseUrl);
       const data = await response.json();
-      console.log(data);
       setInformation({
         ...information,
         city: data.name,
