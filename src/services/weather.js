5b0d54a611d2559f7484d9020a3b44592e7844db
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2025-01-17 11:49:51 -0500

    fix: Fixed a bug that makes the app fails when the user have blocked the location

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,6 +1,10 @@
 import retrieveCityInformation from './location';
 
 export async function fetchWeather(latitude, longitude) {
+  if(latitude === null || longitude === null) {
+    latitude = 40.71427;
+    longitude = -74.00597;
+  }
   const options = {
     method: 'GET',
     headers: { accept: 'application/json' },
@@ -9,6 +13,7 @@ export async function fetchWeather(latitude, longitude) {
     import.meta.env.VITE_OPENWEATHER_API_KEY
   }&units=metric`;
   try {
+    console.log(latitude, longitude);
     const cityInformation = await retrieveCityInformation(latitude, longitude);
     const response = await fetch(baseUrl, options);
     const data = await response.json();
